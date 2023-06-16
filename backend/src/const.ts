06ffbe49939c61d587d4c9445e5c063b32b328be
parsehex <1779937+parsehex@users.noreply.github.com>
2023-06-16 15:42:17 -0400

    export __dirname from const.ts

--- backend/src/const.ts
+++ backend/src/const.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import url from 'url';
 
-const __dirname = url.fileURLToPath(new URL(import.meta.url));
+export const __dirname = url.fileURLToPath(new URL(import.meta.url));
 
 export const THREADS_PATH = path.resolve(__dirname, '../../../../threads');
 export const TTS_PATH = path.join(__dirname, '../../../../tts');
